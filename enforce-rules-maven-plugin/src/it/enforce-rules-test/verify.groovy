111c821f05b1876d2291436ebda55f1060ae125d
Mario Jauvin <marioja@users.noreply.github.com>
2021-05-09 23:02:21 -0400

    added success and failure test. verify.groovy checks both in same log

--- enforce-rules-maven-plugin/src/it/enforce-rules-test/verify.groovy
+++ enforce-rules-maven-plugin/src/it/enforce-rules-test/verify.groovy
@@ -1,3 +1,6 @@
 //File touchFile = new File( basedir, "target/touch.txt" );
 File buildLog = new File((String) basedir, "build.log")
-return buildLog.getText().replace("\r\n","\n").contains("Found Banned Dependency: org.slf4j:slf4j-api:jar:1.7.28");
+return buildLog.getText().replace("\r\n","\n").contains("""Found Banned Dependency: org.slf4j:slf4j-api:jar:1.7.28
+Use 'mvn dependency:tree' to locate the source of the banned dependencies.
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD FAILURE""") && buildLog.getText().replace("\r\n","\n") ==~ /(?s:.)*\[INFO] --- enforce-rules-(?s:.)*\n\[INFO] Executing with maven project(?s:.)*\n\[INFO] ------------------------------------------------------------------------(?s:.)*/;
